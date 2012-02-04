a5962f1fd6bc885daf7de7d7aee1f657372988af
Ashley Towns <ashleyis@me.com>
2012-02-04 19:44:46 +1100

    Update plugins/youtubeurls-plugin/youtubeurls.rb

--- plugins/youtubeurls-plugin/youtubeurls.rb
+++ plugins/youtubeurls-plugin/youtubeurls.rb
@@ -8,7 +8,7 @@ class YoutubeurlsPlugin < Mirai::Plugin
 	end
   
 	def youtube_handler(info, url)
-		title = Nokogiri::HTML(open("http://www.youtube.com/watch?v=T-hDt2E8MoE&feature=g-logo&context=G25fb8cbFOAAAAAAAAAA")).title.strip.split("\n").first
+		title = Nokogiri::HTML(open(url)).title.strip.split("\n").first
 		msg info[:chan], "Youtube: #{title}"
 	end
 end
