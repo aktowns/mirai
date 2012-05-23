f11a746694c33868c386792d4946036719d0f180
Rob McFadzean <github@sphericalcube.net>
2012-05-23 17:59:37 +0930

    Cleaned it up a bit

--- plugins/spotify-plugin/spotify.rb
+++ plugins/spotify-plugin/spotify.rb
@@ -4,31 +4,30 @@ require 'json'
 
 class SpotifyPlugin < Mirai::Plugin
   def on_register
-    add_channel_handler(/(spotify:(album|track|artist):[a-zA-Z0-9]+)/, :spotify_link_handler, :trigger => :none)
-    add_channel_handler(/(http:\/\/open.spotify.com\/(album|track|artist)\/[a-zA-Z0-9]+)/, :spotify_link_handler, :trigger => :none)
+    add_channel_handler(/(spotify:(album|track|artist):[a-zA-Z0-9]+)/, :spotify_link_handler, :trigger => :none, :prefix => :none)
+    add_channel_handler(/(http:\/\/open.spotify.com\/(album|track|artist)\/[a-zA-Z0-9]+)/, :spotify_link_handler, :trigger => :none, :prefix => :none)
   end
 
   def spotify_link_handler info, uri, type
-    puts "URI: #{uri}"
-    puts "Type: #{type}"
     data = get_data(uri)
+    data.empty? ? type = "error" : data = JSON.parse(data)
     case 
     when type.match(/artist/)
-      msg info[:chan], "Artist: #{get_artist(data)}" # Artist: Pewpew
+      msg info[:chan], "Artist: #{get_artist(data)}"
     when type.match(/album/)
       album = get_album(data)
-      msg info[:chan], "#{album['name']} (#{album['released']}) by #{album['artist']} (#{album['artist_id']})" # Blah (2001) by Pewpew (spotify:artist:abc123)
+      msg info[:chan], "#{album['name']} (#{album['released']}) by #{album['artist']} (#{album['artist_id']})" 
     when type.match(/track/)
       track = get_track(data)
-      msg info[:chan], "#{track['name']} by #{track['artist_name']} (#{track['artist_id']}) from the album #{track['album_name']} (#{track['album_id']})" # Zew by Pewpew (spotify:artist:abc123) from the album Blah (spotify:album:abc123)
+      artists = track['artists'].map {|t| "#{artists}#{t['name']} (#{t['href']}), "}.join("")
+      msg info[:chan], "#{track['name']} by #{artists.chomp(', ')} from the album \"#{track['album_name']}\" (#{track['album_id']})"
     else
       msg info[:chan], "Something went wrong."
     end
   end
 
   def get_data uri
-    data = HTTParty.get("http://ws.spotify.com/lookup/1/.json?uri=#{uri}").body
-    return JSON.parse(data) 
+    return HTTParty.get("http://ws.spotify.com/lookup/1/.json?uri=#{uri}").body
   end
 
   def get_artist data
@@ -40,8 +39,7 @@ class SpotifyPlugin < Mirai::Plugin
     info['name'] = data['track']['name']
     info['album_name'] = data['track']['album']['name']
     info['album_id'] = data['track']['album']['href']
-    info['artist_name'] = data['track']['artists'][0]['name'] # TODO: Multiple artist support
-    info['artist_id'] = data['track']['artists'][0]['href']
+    info['artists'] = data['track']['artists']
     return info
   end
 
